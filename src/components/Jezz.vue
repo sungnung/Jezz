46d996aa12a03f18e0b43ada839a6a0f4521c609
Danihoon <danihoooon@gmail.com>
2020-05-24 17:15:26 +0900

    Update Timeline Form

--- src/components/Jezz.vue
+++ src/components/Jezz.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <v-card class="overflow-hidden" flat>
-            <v-app-bar absolute :color="color" dark shrink-on-scroll elevate-on-scroll prominent scroll-target="#scrolling-techniques">
-                <v-toolbar-title :class="'title_font' ">{{Title.text}} {{height}}</v-toolbar-title>
+            <v-app-bar absolute :color="color" dark shrink-on-scroll elevate-on-scroll prominent scroll-target="#scrolling-techniques" style="z-index:10">
+                <v-toolbar-title :class="'title_font' ">{{Title.text}}
+                    <span v-if="days===0">&#128518</span>
+                    <span v-else-if="days===1">&#1F60E</span>
+                    <span v-else-if="days===2">&#128540</span>
+                    <span v-else-if="days===3">&#1F60D</span>
+                </v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-btn icon>
                     <v-icon>mdi-map</v-icon>
@@ -40,7 +45,7 @@ export default {
     data () {
         return {
             date: new Date,
-            days:0,
+            days:1,
             window:{
                 height:600,
                 width:600,
@@ -64,10 +69,10 @@ export default {
         },
         Title () {
             switch (this.days) {
-                case 0: return {text:'Preparation', color:'primary--text text--accent-4'}
-                case 1: return {text:'1st Day :)', color:'teal--text text--accent-4'}
-                case 2: return {text:'2nd Day:)', color:'pink--text text--accent-4'}
-                case 3: return {text:'3rd Day :)', color:'indigo--text text--accent-4'}
+                case 0: return {text:'Preparation', icon:'&#128518', color:'primary--text text--accent-4'}
+                case 1: return {text:'1st Day', icon:'&#1F60E', color:'teal--text text--accent-4'}
+                case 2: return {text:'2nd Day', icon:'&#128540', color:'pink--text text--accent-4'}
+                case 3: return {text:'3rd Day', icon:'&#1F60D', color:'indigo--text text--accent-4'}
             }
         }
     },
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,24 +1,41 @@
 <template>
-    <v-timeline>
-      <v-timeline-item
-        v-for="(year, i) in years"
-        :key="i"
-        :color="year.color"
-        small
-      >
-        <template v-slot:opposite>
-          <span
-            :class="`headline font-weight-bold ${year.color}--text`"
-            v-text="year.year"
-          ></span>
-        </template>
-        <div class="py-4">
-          <h2 :class="`headline font-weight-light mb-4 ${year.color}--text`">Lorem ipsum</h2>
-          <div>
-            Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-          </div>
-        </div>
-      </v-timeline-item>
+    <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
+        <v-timeline-item v-for="(item, i) in timeline" :key="i" :color="color_by_type(item)" :icon="item.icon" fill-dot>
+            <v-card v-if="!item.is_done" :color="color_by_type(item)" dark>
+                <v-card-title class="title">{{item.time}} {{item.title}}</v-card-title>
+                <v-card-text class="white text--primary">
+                    <p>Lorem ipsum dolor</p>
+                    <v-btn :color="color_by_type(item)" class="mx-0" outlined @click="item.is_done=true">
+                        Finished
+                    </v-btn>
+                </v-card-text>
+            </v-card>
+
+            <v-expand-transition>
+            <v-card v-show="item.is_done">
+                <v-carousel height="200" :continuous="false" :cycle="cycle" :show-arrows="false" hide-delimiter-background delimiter-icon="mdi-minus">
+                    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+                        <v-sheet :color="colors[i]" height="100%" tile>
+                            <v-row class="fill-height" align="center" justify="center">
+                                <div class="display-3">{{ slide }} Slide</div>
+                            </v-row>
+                        </v-sheet>
+                    </v-carousel-item>
+                </v-carousel>
+                <v-list two-line>
+                    <v-list-item>
+                        <v-list-item-avatar>
+                            <v-icon>{{item.icon}}</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>{{item.type}}</v-list-item-title>
+                            <v-list-item-subtitle>{{item.title}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-card>
+        </v-expand-transition>
+        </v-timeline-item>
     </v-timeline>
 </template>
 
@@ -28,33 +45,100 @@ export default {
     name: 'Timeline',
     components: {
     },
+    props:{
+        day:{
+            type:Number,
+            default:1,
+        }
+    },
     data () {
         return {
-          years: [
-      {
-        color: 'cyan',
-        year: '1960',
-      },
-      {
-        color: 'green',
-        year: '1970',
-      },
-      {
-        color: 'pink',
-        year: '1980',
-      },
-      {
-        color: 'amber',
-        year: '1990',
-      },
-      {
-        color: 'orange',
-        year: '2000',
-      },
-    ],
+            first_timeline: [
+                {
+                    type: 'Tour',
+                    time: '09:00',
+                    title:'비행기 탑승',
+                    icon:'mdi-airplane',
+                    place:'',
+                    is_done:false,
+                },
+                {
+                    type: 'Activity',
+                    time: '10:30',
+                    title:'렌트카 대여',
+                    icon:'mdi-car',
+                    place:'',
+                    is_done:false,
+                },
+                {
+                    type: 'Meal',
+                    time: '1980',
+                    title:'비행기 탑승',
+                    icon:'mdi-airplane',
+                    place:'',
+                    is_done:false,
+                },
+                {
+                    type: 'Dessert',
+                    time: '1990',
+                    title:'비행기 탑승',
+                    icon:'mdi-airplane',
+                    place:'',
+                    is_done:false,
+                },
+                {
+                    type: 'Accomodation',
+                    time: '2000',
+                    title:'비행기 탑승',
+                    icon:'mdi-airplane',
+                    place:'',
+                    is_done:false,
+                },
+            ],
+
+            // Temp
+            colors: [
+                  'green',
+                  'secondary',
+                  'yellow darken-4',
+                  'red lighten-2',
+                  'orange darken-1',
+                ],
+                cycle: false,
+                slides: [
+                  'First',
+                  'Second',
+                  'Third',
+                  'Fourth',
+                  'Fifth',
+                ],
         }
     },
     computed: {
+        timeline (){
+            if(this.day === 1) return this.first_timeline
+            else if(this.day === 2) return this.second_timeline
+            else if(this.day === 3) return this.third_timeline
+        }
+    },
+    methods:{
+        // Utils
+        color_by_type (item){
+            if (item.is_done) return 'grey lighten-2'
+
+
+            let items = {
+                Tour:'#B57082', // 관광
+                Activity:'#D06380', // 체험
+                Meal:'#ED4C00', // 식사
+
+                Dessert:'#63453D', // 디저트
+                Accomodation:'#2B242C', // 숙소
+            }
+
+            return items[item.type]
+
+        }
     }
 }
 </script>
