2ff73828a3a6bc8b24597048502171d7dcd58cf2
Danihoon <danihoooon@gmail.com>
2020-05-25 15:25:23 +0900

    Update Icon

---
+++ docs/precache-manifest.562c268fa08b78f23273f2b76d7bc9a8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d475150feba5b323220e",
+    "url": "/Jezz/css/app.b4b17e7f.css"
+  },
+  {
+    "revision": "c06d7a0712080433961f",
+    "url": "/Jezz/css/chunk-vendors.d02ef32f.css"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/Jezz/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/Jezz/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/Jezz/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/Jezz/fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "21b4e38abbdb45d4a872337859c779b8",
+    "url": "/Jezz/img/0.21b4e38a.png"
+  },
+  {
+    "revision": "44cfd08ba7f6712f8d94765ecf5f418e",
+    "url": "/Jezz/img/1.44cfd08b.png"
+  },
+  {
+    "revision": "4189bb941ec517ee337473054217594b",
+    "url": "/Jezz/img/2.4189bb94.png"
+  },
+  {
+    "revision": "bdae6422ef10dd0bbfa55ae34f793420",
+    "url": "/Jezz/img/3.bdae6422.png"
+  },
+  {
+    "revision": "4f0da41480655d0faeb148f9b03ec39e",
+    "url": "/Jezz/img/JeJuMap.4f0da414.png"
+  },
+  {
+    "revision": "1b33bc34c3ccb2a0596e732dc6c565d3",
+    "url": "/Jezz/img/dani.1b33bc34.png"
+  },
+  {
+    "revision": "1b33bc34c3ccb2a0596e732dc6c565d3",
+    "url": "/Jezz/img/dani.png"
+  },
+  {
+    "revision": "c72d667606b9a7b60e697474fa3efae3",
+    "url": "/Jezz/img/favicon.png"
+  },
+  {
+    "revision": "8d1c7f2f7801dd290652bf7d54701cb7",
+    "url": "/Jezz/img/logo.png"
+  },
+  {
+    "revision": "3930be7b5f778b639adf1a263eca3fa5",
+    "url": "/Jezz/img/may.3930be7b.png"
+  },
+  {
+    "revision": "3930be7b5f778b639adf1a263eca3fa5",
+    "url": "/Jezz/img/may.png"
+  },
+  {
+    "revision": "fe8eb49abda3f6672c41a4eb71a5992f",
+    "url": "/Jezz/index.html"
+  },
+  {
+    "revision": "d475150feba5b323220e",
+    "url": "/Jezz/js/app.212af0c0.js"
+  },
+  {
+    "revision": "c06d7a0712080433961f",
+    "url": "/Jezz/js/chunk-vendors.34b61470.js"
+  },
+  {
+    "revision": "eb89e4d22cfd381d14c0b6875eae092c",
+    "url": "/Jezz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Jezz/robots.txt"
+  }
+]);
